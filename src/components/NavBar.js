f178a3c40b5aa9fdf09afe8bded6ba9b25e65149
maina <ian.maina@student.moringaschool.com>
2024-03-01 17:17:02 +0300

    new logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import hhalogo from "../images/KenyaLogos.png"
+import hhalogo from "../images/kelogos.png"
 
 function NavBar() {
 
@@ -16,7 +16,7 @@ function NavBar() {
   }
 
   return (
-    <nav className='mx-auto flex h-30 text-black bg-pink-50'>
+    <nav className='mx-auto flex h-30 text-black bg-red-50'>
       <div className='flex flex-row md:w-2/3 space-x-56'>
       <div className='mx-auto w-1/5' >
         <img  onClick={handleNavigation} src={hhalogo} alt="logo"/>
