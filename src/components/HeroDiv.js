fd074869122974e3471516c1db62a7a5a3f5a398
JeffMuthunga <jeffmuthunga@gmail.com>
2024-04-07 12:34:34 +0300

    Round

--- src/components/HeroDiv.js
+++ src/components/HeroDiv.js
@@ -26,7 +26,7 @@ function HeroDiv() {
     </div>
 
   <div className="flex flex-row justify-center mb-8 sm:justify-start space-x-8 md:space-x-4">
-  <button className="bg-green-500 h-12 w-32 text-sm text-white rounded-lg"> <Link to="/contact-us" >Contact us</Link></button>
+  <button className="bg-green-500 h-12 w-32 text-sm text-white rounded-lg "> <Link to="/contact-us" >Contact us</Link></button>
   <button className=" h-12 w-32 text-sm">
    <Link to="/products" className=' flex gap-x-2 items-center'> Designs <FaLongArrowAltRight className='text-base' /> </Link>
   </button>
